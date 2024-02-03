8fa2e3747b41ee6e1dc9e91c3c71f69aed925e7b
gian_jamisola <gcjamisola@gmail.com>
2024-02-03 23:07:07 +0800

    Try web deployment: added Railway env variables

--- server/db.ts
+++ server/db.ts
@@ -1,11 +1,16 @@
 const POOL = require('pg').Pool
 
 const pool = new POOL({
-    user: 'postgres',
-    password: 'superadmin1234',
-    host: 'localhost',
-    port: 5432,
-    database: 'duty_db'
+    // user: 'postgres',
+    // password: 'superadmin1234',
+    // host: 'localhost',
+    // port: 5432,
+    // database: 'duty_db'
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE
 })
 
 module.exports = pool
