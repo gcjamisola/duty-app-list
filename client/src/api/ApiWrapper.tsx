19a42321ca61909eb255327994878bbeead0cb61
gian_jamisola <gcjamisola@gmail.com>
2024-02-04 00:17:24 +0800

    use old way of referencing env

--- client/src/api/ApiWrapper.tsx
+++ client/src/api/ApiWrapper.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND.URL
+const BACKEND_URL = process.env.BACKEND_URL
 const client = axios.create({ baseURL: BACKEND_URL })
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
